f9bed443b3c78f6269013c14cf34f23aebcde067
Miri Hyman <Miri.hyman@wildnoodle.com>
2018-01-28 15:40:57 -0800

    updated app

--- capstone-seed/app.js
+++ capstone-seed/app.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const app = express();
-const video = require('./router.js');
 
 // set up an arbitrary port to access on localhost
 const port = 8081;
 
 // make a call to handle any possible direction
-app.use('/', video);
 
 
 app.listen(port, () => {
