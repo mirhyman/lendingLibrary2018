5b0f13ff8b27359b41b687a84c64ba3bea0eab5c
Miri Hyman <Miri.hyman@wildnoodle.com>
2018-03-01 12:31:40 -0800

    yo idk

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const bodyparser = require('body-parser');
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true}));
+
+// set up an arbitrary port to access on localhost
+const port = process.env.PORT || 8081;
+
+// make a call to handle any possible direction
+const route = require('src/routers/router.js');
+
+app.use(express.static('src/public'));
+app.use('/loadProduct', express.static('src/public/product.html'));
+app.use('/productPage', express.static('src/public/productPage.html'));
+app.use('/saveProducts', express.static('src/public/saveProducts.html'));
+app.use('/guidedSearch', express.static('src/public/guidedSearch.html'));
+app.use('/compareProducts', express.static('src/public/compare.html'));
+app.use('/glossary', express.static('src/public/glossary.html'));
+app.use('/', route);
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+});
+
+module.exports = app;
