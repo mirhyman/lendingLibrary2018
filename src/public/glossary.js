8ff97ae1fa60d92bb2dc847d0eba5263077b7fec
Miri Hyman <Miri.hyman@wildnoodle.com>
2018-05-27 13:59:21 -0700

    fix link

--- src/public/glossary.js
+++ src/public/glossary.js
@@ -1,3 +1,14 @@
 function goHome() {
     window.location = "/";
 }
+
+function allProducts() {
+    let oReq = new XMLHttpRequest();
+
+    oReq.open("GET", "/products", true);
+    oReq.onload = function(oEvent) {
+        window.location = "/loadProduct?";
+        console.log(oReq.responseText);
+    };
+    oReq.send();
+}
